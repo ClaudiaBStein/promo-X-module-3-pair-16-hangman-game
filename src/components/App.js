492193bd781a1ef705542e5fa6a886555aeedbb1
Sararid <sarawen88@gmail.com>
2021-09-17 11:11:01 +0200

    corrected dev tools errors

--- src/components/App.js
+++ src/components/App.js
@@ -36,11 +36,11 @@ function App() {
             </ul>
           </div>
           <form className="form">
-            <label className="title" for="last-letter">Escribe una letra:</label>
+            <label className="title" htmlFor="last-letter">Escribe una letra:</label>
             <input
-              autocomplete="off"
+              autoComplete="off"
               className="form__input"
-              maxlength="1"
+              maxLength="1"
               type="text"
               name="last-letter"
               id="last-letter"
