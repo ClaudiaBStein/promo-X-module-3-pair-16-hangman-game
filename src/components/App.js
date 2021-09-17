7b59af174bdf047a2bbfc1f7e2af5074cef51483
Sararid <sarawen88@gmail.com>
2021-09-17 10:35:41 +0200

    add react starter kit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+// Fichero src/components/App.js
+
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div>
+      <h1 className="title">Hola mundo 2 + 20</h1>
+    </div>
+  );
+}
+
+export default App;
